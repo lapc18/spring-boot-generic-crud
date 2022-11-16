29cf4a3ae1d964176a77d445e146d2621f9f5edd
lapc18 <lapc18@outlook.com>
2022-11-16 12:22:32 -0400

    initial commit

---
+++ src/main/java/io/github/lapc18/springbootgenericcrud/SpringBootGenericCrudApplication.java
@@ -0,0 +1,13 @@
+package io.github.lapc18.springbootgenericcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGenericCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGenericCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/lapc18/springbootgenericcrud/SpringBootGenericCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lapc18.springbootgenericcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGenericCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
